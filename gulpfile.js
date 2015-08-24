f4053cb012b5e55d43396f20c51d7423cbcf687f
erick2014 <erickarmandogarcia@hotmail.com>
2015-08-24 11:32:27 -0500

    Add gulpfile,package.json and .gitingore files.

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp=require('gulp');
+var browserify=require("browserify");
+var babelify=require("babelify");
+var source=require('vinyl-source-stream');
+
+
+gulp.task('build',function(){
+	browserify({
+		entries:'app.js',
+		extensions:['.jsx'],
+		debug:true
+	})
+	.transform(babelify)
+	.bundle()
+	.pipe(source('bundle.js'))
+	.pipe(gulp.dest('dist'));
+})
+
+gulp.task('default',['build']);
