08d3ccc7de83b3ef995d69938360443e6bcfafe9
erick2014 <erickarmandogarcia@hotmail.com>
2015-09-11 13:34:43 -0500

    Fix indentation

--- stores/ListStore.js
+++ stores/ListStore.js
@@ -2,20 +2,20 @@ import {EventEmitter} from 'events';
 import _ from 'lodash';
 
 let ListStore=_.extend({},EventEmitter.prototype,{
-	//Mock default data
-	items:[
-		{
-			name:'Item 1',
-			id:0
-		},
-		{
-			name:'Item 2',
-			id:1
-		}
-	],
-	getItems:function(){
-		return this.items;
-	}
+  //Mock default data
+  items:[
+    {
+      name:'Item 1',
+      id:0
+    },
+    {
+      name:'Item 2',
+      id:1
+    }
+  ],
+  getItems:function(){
+    return this.items;
+  }
 
 })
 
